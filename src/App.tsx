be41242dda34af04a34ec941d4eece3ff5008afb
UGSNBandara <sulithanb119@gmail.com>
2025-11-02 11:55:33 +0530

    add the sofia model

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { useGLTF, PerspectiveCamera } from '@react-three/drei'
+import { Suspense } from 'react'
 
-function App() {
+function AvatarOnly() {
+  const { scene } = useGLTF('/avatar.glb')
+  return <primitive object={scene} />
+}
+
+export default function App() {
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
       <Canvas>
-        <OrbitControls />
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
+        <PerspectiveCamera makeDefault position={[0, 1.5, 2]} fov={50} />
+        <ambientLight intensity={0.8} />
+        <directionalLight position={[5, 10, 5]} intensity={1} />
+        <Suspense fallback={null}>
+          <AvatarOnly />
+        </Suspense>
       </Canvas>
     </div>
   )
 }
-
-export default App
